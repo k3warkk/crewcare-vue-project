80543359f20e56b51762c6b555e82acffbe17195
k3warkk <rendyl_brian_bendanillo@dlsl.edu.ph>
2024-11-21 16:07:49 +0800

    test

---
+++ src/locales.js
@@ -0,0 +1,14 @@
+export default {
+  en: {
+    welcome: "Welcome",
+    logout: "Logout",
+    captureImage: "Capture Image",
+    stopWebcam: "Stop Webcam",
+  },
+  es: {
+    welcome: "Bienvenido",
+    logout: "Cerrar sesión",
+    captureImage: "Capturar imagen",
+    stopWebcam: "Detener cámara",
+  },
+};
--- src/views/AdminHome.vue
+++ src/views/AdminHome.vue
@@ -1,12 +1,15 @@
 <template>
+  <div>
+    <router-link to="permitlist"><button>List</button></router-link>
+    <router-link to="access-logs"><button>Access Logs</button></router-link>
+    <p>User Token: {{ userToken }}</p>
+    <button @click="logout">Logout</button>
     <div>
-        <router-link to="permitlist"><button>List</button></router-link>
-        <router-link to="access-logs"><button>Access Logs</button></router-link>
-        <p>User Token: {{ userToken }}</p>
-        <button @click="logout">Logout</button>
-        <router-link to="login"><button>Login</button></router-link>
-        <router-link to="signup"><button>Signup</button></router-link>
+      <button @click="activateMotor(1)">Activate Motor 1</button>
+      <button @click="activateMotor(2)">Activate Motor 2</button>
+      <p v-if="message">{{ message }}</p>
     </div>
+  </div>
 </template>
 
 <script>
@@ -15,42 +18,57 @@ import axiosClient from "../axios";
 import { useRouter } from "vue-router";
 
 export default {
-    setup() {
-        const router = useRouter();
+  setup() {
+    const router = useRouter();
 
-        const stateContext = inject("stateContext");
-        if (!stateContext) {
-            throw new Error("StateContext is not provided");
-        }
+    const stateContext = inject("stateContext");
+    if (!stateContext) {
+      throw new Error("StateContext is not provided");
+    }
 
-        const {
-            userToken,
-            setUserToken,
-            currentUser,
-            setCurrentUser,
-            setUserRole,
-        } = stateContext;
+    const {
+      userToken,
+      setUserToken,
+      currentUser,
+      setCurrentUser,
+      setUserRole,
+    } = stateContext;
 
-        const logout = () => {
-            axiosClient.post("/logout").then((res) => {
-                setCurrentUser({});
-                setUserToken(null);
-                setUserRole(null);
-            });
-        };
+    const logout = () => {
+      axiosClient.post("/logout").then((res) => {
+        setCurrentUser({});
+        setUserToken(null);
+        setUserRole(null);
+      });
+    };
 
-        watchEffect(() => {
-            if (!userToken.value) {
-                router.push({ name: "Login" });
-            }
+    watchEffect(() => {
+      if (!userToken.value) {
+        router.push({ name: "Login" });
+      }
+    });
+
+    const message = ref("");
+
+    const activateMotor = (motorId) => {
+      axiosClient
+        .post("/trigger-motor", { motor_id: motorId })
+        .then((response) => {
+          message.value = response.data.message;
+        })
+        .catch((error) => {
+          message.value = "Error activating motor";
         });
+    };
 
-        return {
-            currentUser,
-            userToken,
-            logout,
-        };
-    },
+    return {
+      currentUser,
+      userToken,
+      logout,
+      activateMotor,
+      message,
+    };
+  },
 };
 </script>
 
